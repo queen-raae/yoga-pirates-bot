507cde0750ff0e9fae54027d6f05f00341c3c22a
Benedicte Raae <raae@raae.codes>
2023-01-01 21:29:14 +0100

    remove unused env var

--- src/config.js
+++ src/config.js
@@ -2,4 +2,3 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export const DISCORD_CLIENT_TOKEN = process.env.DISCORD_CLIENT_TOKEN;
-export const DISCORD_CLIENT_ID = process.env.DISCORD_CLIENT_ID;
