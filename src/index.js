2b3be3fc8cab05a4083bcebc926717e838e8840c
Benedicte Raae <raae@raae.codes>
2023-01-01 19:06:00 +0100

    pof xata

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 // Require the necessary discord.js classes
 const { Client, Events, GatewayIntentBits } = require("discord.js");
+const { addDays } = require("date-fns");
 
 const { DISCORD_CLIENT_TOKEN } = require("./config");
+const { getXataClient } = require("./xata");
 
-// Create a new client instance
-const client = new Client({
+// Create a new discord client instance
+const discordClient = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
@@ -12,27 +14,54 @@ const client = new Client({
   ],
 });
 
+// create a new xata client instance
+const xata = getXataClient();
+
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, (c) => {
+discordClient.once(Events.ClientReady, (c) => {
   console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
 // When there is a yoga log message, reply with
-client.on(Events.MessageCreate, async (message) => {
+discordClient.on(Events.MessageCreate, async (message) => {
   console.log({ message });
   // Get channel
-  const channel = await client.channels.fetch(message.channelId);
+  const channel = await discordClient.channels.fetch(message.channelId);
+  const messageContent = message.content.trim();
 
   // Ignore bot messages
   if (message.author.bot) return;
   // Ignore if not in yoga channel
   if (channel.name.toLowerCase() !== "yoga") return;
   // Ignore if message does not start with ✅
-  if (!message.content.trim().startsWith("✅")) return;
+  if (!messageContent.startsWith("✅")) return;
+
+  // This should be "✅" or "✅-3" etc
+  const logContent = messageContent.split(" ")[0];
+
+  // When removing ✅ from logContent should have "-3", "-1", "" etc
+  const timeShiftNumber = parseInt(logContent.replace("✅", ""));
+
+  const creationDate = new Date(message.createdTimestamp);
+  let logDate = creationDate;
+
+  if (!isNaN(timeShiftNumber)) {
+    logDate = addDays(logDate, timeShiftNumber);
+  }
+
+  const record = await xata.db.log.create({
+    id: message.id,
+    creationDate: creationDate,
+    logDate: logDate,
+    description: messageContent.replace(logContent, "").trim(),
+    discordUserId: message.author.id,
+  });
+
+  console.log({ record });
 
   message.react("🏴‍☠️");
 });
 
 // Log in to Discord with your client's token
-client.login(DISCORD_CLIENT_TOKEN);
+discordClient.login(DISCORD_CLIENT_TOKEN);
