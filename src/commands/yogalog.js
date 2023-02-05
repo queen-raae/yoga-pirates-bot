6b3fff6ddf7cc84f99a1c4639902fd372065acd2
David Wessman <david.wesmn@gmail.com>
2023-02-05 18:11:10 +0100

    Adds filtering on week, month and year

--- src/commands/yogalog.js
+++ src/commands/yogalog.js
@@ -4,15 +4,55 @@ import { getXataClient } from "./../xata.js";
 export default {
   data: new SlashCommandBuilder()
     .setName('yogalog')
-    .setDescription('Lists all your yoga sessions'),
+    .setDescription('Lists all your yoga sessions')
+    .addStringOption(option =>
+      option.setName('period')
+        .setDescription("Which period to list")
+        .setRequired(false)
+        .addChoices(
+          {
+            name: "Week",
+            value: "week"
+          },
+          {
+            name: "Month",
+            value: "month"
+          },
+          {
+            name: "Year",
+            value: "year"
+          }
+        )
+    ),
+
   async execute(interaction, xata = getXataClient()) {
     const discordUserId = interaction.user.id;
+    const period = interaction.options.getString('period');
 
-    const results = await xata.db.session
+    let results = await xata.db.session
       .filter({discordUserId: discordUserId})
-      .getAll();
 
+    if (period) {
+      const start = new Date();
+      switch (period) {
+        case "week":
+          start.setDate(start.getDate() - 7);
+          break;
+        case "month":
+          start.setMonth(start.getMonth() - 1);
+          break;
+        case "year":
+          start.setFullYear(start.getFullYear() - 1);
+          break;
+      }
+      results = results.filter({sessionTimestamp: {$gt: start}});
+    }
+
+    results = await results.getAll();
     let output = `Days of yoga: ${results.length}\n`
+    if (period) {
+      output += `Period: ${period}\n`
+    }
     output += results.map(row => `- ${row.sessionDateString}: ${row.note}`).join("\n")
 
     await interaction.reply(output);
