d55824868118c3b60dcaae8d487b8c6d737fc55a
David Wessman <david.wesmn@gmail.com>
2023-02-25 21:20:12 +0100

    fix: Handles long messages in /yogalog (#19)

--- src/commands/yogalog.js
+++ src/commands/yogalog.js
@@ -3,34 +3,36 @@ import { getXataClient } from "./../xata.js";
 
 export default {
   data: new SlashCommandBuilder()
-    .setName('yogalog')
-    .setDescription('Lists all your yoga sessions')
-    .addStringOption(option =>
-      option.setName('period')
+    .setName("yogalog")
+    .setDescription("Lists all your yoga sessions")
+    .addStringOption((option) =>
+      option
+        .setName("period")
         .setDescription("Which period to list")
         .setRequired(false)
         .addChoices(
           {
             name: "Week",
-            value: "week"
+            value: "week",
           },
           {
             name: "Month",
-            value: "month"
+            value: "month",
           },
           {
             name: "Year",
-            value: "year"
+            value: "year",
           }
         )
     ),
 
   async execute(interaction, xata = getXataClient()) {
     const discordUserId = interaction.user.id;
-    const period = interaction.options.getString('period');
+    const period = interaction.options.getString("period");
 
-    let results = await xata.db.session
-      .filter({discordUserId: discordUserId})
+    let results = await xata.db.session.filter({
+      discordUserId: discordUserId,
+    });
 
     if (period) {
       const start = new Date();
@@ -45,16 +47,43 @@ export default {
           start.setFullYear(start.getFullYear() - 1);
           break;
       }
-      results = results.filter({sessionTimestamp: {$gt: start}});
+      results = results.filter({ sessionTimestamp: { $gt: start } });
     }
 
-    results = await results.getAll();
-    let output = `Days of yoga: ${results.length}\n`
+    results = await results.sort("sessionTimestamp", "asc").getAll();
+    let header = `Days of yoga: ${results.length}\n`;
     if (period) {
-      output += `Period: ${period}\n`
+      header += `Period: ${period}\n`;
     }
-    output += results.map(row => `- ${row.sessionDateString}: ${row.note}`).join("\n")
 
-    await interaction.reply({ content: output, ephemeral: true});
+    const rows = results.map((row) => {
+      const result = `- ${row.sessionDateString}: ${row.note}`;
+      if (result.length > 2000) {
+        return result.slice(0, 2000);
+      }
+
+      return result;
+    });
+
+    const totalLength = rows.reduce((acc, row) => acc + row.length, 0);
+    if (totalLength + header.length > 2000) {
+      const reply = `${header}Too many rows to display, will send as DM.`;
+      await interaction.reply({ content: reply, ephemeral: true });
+      let message = header;
+      for (const row of rows) {
+        if (message.length + row.length > 2000) {
+          await interaction.user.send(message);
+          message = "";
+        }
+        message += row + "\n";
+      }
+
+      await interaction.user.send(message);
+    } else {
+      await interaction.reply({
+        content: `${header}${rows.join("\n")}`,
+        ephemeral: true,
+      });
+    }
   },
 };
